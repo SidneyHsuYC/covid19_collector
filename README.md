bfef872115d6df1efeb725ea85aee5f3b796462d
SidneyHsuYC <ych005@eng.ucsd.edu>
2020-07-10 09:42:13 -0700

    Fixed README error

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Empty folders including farjan_repo, ahmad_repo, ieee8032_repo, simon_repo and b
 are repositories of COVID_19 dataset
 Please change to each repository: `cd {repository name}`
 And do
-1. Farjan_repo: `git clone https://github.com/Farjanasumona/covid_chestXray_dataset/tree/master/covid_19%20dataset`
+1. Farjan_repo: `git clone https://github.com/Farjanasumona/covid_chestXray_dataset`
 2. ieee8032_repo: `git clone https://github.com/ieee8023/covid-chestxray-dataset`
 3. bimcv_repo: TBD
-4. sirm_repo: `python3 simon_repo_downloader.py`
+4. sirm_repo: `python3 sirm_repo_downloader.py`
 5. ahmad_repo: `git clone https://github.com/ahmadhassan7/Covid-19-Datasets`
--- dataset_builder.py
+++ dataset_builder.py
@@ -244,18 +244,18 @@ def main():
 
     ieee0832_path = Path.cwd() / 'ieee8032_repo'
     os.chdir(ieee0832_path)
-    # ieee_builder = IEEE8023_builder('metadata.csv')
+    ieee_builder = IEEE8023_builder('metadata.csv')
 
-    # ieee_builder._load_dataset()
-    # ieee_dataset = ieee_builder._dataset
-    # print(f"ieee dataset images quantity: {len(ieee_dataset)}")
+    ieee_builder._load_dataset()
+    ieee_dataset = ieee_builder._dataset
+    print(f"ieee dataset images quantity: {len(ieee_dataset)}")
 
     farjan_path = Path.cwd().parent / 'farjan_repo' 
     os.chdir(farjan_path)
-    # farjan_builder = Farjan_builder()
-    # farjan_builder._load_dataset()
-    # farjan_dataset = farjan_builder._dataset
-    # print(f"farjan dataset images quantity: {len(farjan_dataset)}")
+    farjan_builder = Farjan_builder()
+    farjan_builder._load_dataset()
+    farjan_dataset = farjan_builder._dataset
+    print(f"farjan dataset images quantity: {len(farjan_dataset)}")
 
     sirm_path = Path.cwd().parent / 'sirm_repo' 
     os.chdir(sirm_path)
@@ -264,8 +264,6 @@ def main():
     sirm_dataset = sirm_builder._dataset
     print(f"sirm_path dataset images quantity: {len(sirm_dataset)}")
     print(len(sirm_dataset))
-    for i in sirm_dataset:
-        print(i)
 
 if __name__ == '__main__':
     main()
